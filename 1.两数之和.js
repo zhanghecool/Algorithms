a76072c7ff484d8d5961c7be60efc3a41f990f03
He Zhang <zhanghecool@outlook.com>
2020-09-30 18:05:02 +0800

    feat(add): 增加两数之和算法题解

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -8,7 +8,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+  const m = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const current = nums[i];
+    const another = target - current;
+    if (m.has(another)) {
+      return [m.get(another), i];
+    }
+    m.set(current, i);
+  }
 };
-
